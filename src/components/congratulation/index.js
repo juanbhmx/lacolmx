513b6d27d4f55490433ef14e126c008504f40458
juanbhmx <fugus.dks@gmail.com>
2020-12-03 20:55:25 -0600

    compra

--- src/components/congratulation/index.js
+++ src/components/congratulation/index.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import Checkout from '../checkout';
 import '../login/style.scss';
+import Axios from "axios";
+
+const App = props => {
+
+const ale=()=>{
+  alert("En un instante se verifica los datos de la compra: Gracias!");
+  window.location.href="/historialpay"
+}
+  return (
+    <header class="masthead d-flex">
+      <div class="container text-center my-auto">
+        <h1 class="mb-1">Confirmar Pago</h1>
+        <h3 class="mb-5">
+          <em>Gracias por su preferencia!</em>
+          {/*modificacion*/}
+          <button
+            onClick={ale}
+            href="/"
+            class="btn btn-danger">
+            <i class="fa fa-shopping-cart">
+            </i>Confirmar Compra
+                            </button>
 
 
-const CartPrice = props => {
+          {/*modificacion*/}
 
-    return (
-        <header class="masthead d-flex">
-        <div class="container text-center my-auto">
-          <h1 class="mb-1">Compra Exitosa</h1>
-          <h3 class="mb-5">
-            <em>Gracias por su preferencia!</em>
-          </h3>
-          <a class="btn btn-primary btn-xl js-scroll-trigger" href="/">Ver Mas productos</a>
-        </div>
-        <div class="overlay"></div>
-      </header>
-    )
+        </h3>
+
+        {/* <a class="btn btn-primary btn-xl js-scroll-trigger" href="/">Confirmar pago</a> */}
+      </div>
+      <div class="overlay"></div>
+    </header>
+  );
 }
 
-export default CartPrice;
+export default App;
